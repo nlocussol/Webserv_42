95d01e579f629d986b7f9bcb0a0007b58342dd7f
Armandase <adamiens@student.42angouleme.fr>
2023-04-27 10:15:29 +0200

    file open and exec protection for handle cgi func

--- srcs/handle_cgi.cpp
+++ srcs/handle_cgi.cpp
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int	handle_cgi(std::string inter, std::string exec);
 
@@ -11,14 +13,38 @@ int main ()
 	std::string path("/usr/bin/python3");
 	std::string file("test.py");
 	int			fd;
-	std::ostringstream	ret;
 
 	fd = handle_cgi(path, file);
+	if (fd == -1)
+		return (1);
 	char buf[2600] = {0};
 	if(read(fd, buf, 2600))
-		std::cout << buf << std::endl;
-	std::cout << "error";
+		std::cout << buf;
+	else
+		std::cout << "error";
 	close(fd);
+	return (0);
+}
+
+int	check_cgi_args(std::string inter, std::string exec)
+{
+	int			fd;
+
+	fd = open(exec.c_str(), O_RDWR);
+	if (fd == -1)
+	{
+		std::cout << "Can't use " << exec << " as an exectuable." << std::endl;
+		return (fd);
+	}
+	close (fd);
+	fd = access(inter.c_str(), X_OK);
+	if (fd == -1)
+	{
+		std::cout << "Can't use " << inter << " as an interpreter." << std::endl;
+		return (fd);
+	}
+	close (fd);
+	return (0);
 }
 
 int	handle_cgi(std::string inter, std::string exec)
@@ -28,6 +54,8 @@ int	handle_cgi(std::string inter, std::string exec)
 	int	wstatus;
 	char	*param[3] = {(char *)inter.c_str(), (char *)exec.c_str(), NULL};
 
+	if (check_cgi_args(inter, exec) == -1)
+		return (-1);
 	pipe(pip);
 	pid = fork();
 	if (pid == -1)
