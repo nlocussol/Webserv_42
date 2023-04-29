228254b3a80705f0fce1633e854a8b0df2a1a623
Armandase <adamiens@student.42angouleme.fr>
2023-04-29 12:59:48 +0200

    client timout update

--- srcs/client/client.go
+++ srcs/client/client.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"io/ioutil"
 	"fmt"
 	"net/http"
 	"time"
@@ -11,29 +10,27 @@ import (
 func main() {
 	sum := 1
 	for sum < 5000{
-		c := http.Client{Timeout: time.Duration(1) * time.Second}
+		c := http.Client{Timeout: time.Duration(1) * time.Second * 100}
 		resp, err := c.Get("http://127.0.0.1:16161")
 		if err != nil {
 			fmt.Printf("Error %s", err)
 			return
 		}
 		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-		fmt.Printf("Body : %s", body)
+		fmt.Printf("GET package: %d\n", sum)
 		sum += 1;
 	}
 	sum = 1
 	file := bytes.NewBuffer([]byte(`{"name":"SubwaySurfer"}`))
 	for sum < 5000{
-		c := http.Client{Timeout: time.Duration(1) * time.Second}
+		c := http.Client{Timeout: time.Duration(1) * time.Second * 100}
 		resp, err := c.Post("http://127.0.0.1:16161", "okok", file)
 		if err != nil {
 			fmt.Printf("Error %s", err)
 			return
 		}
 		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-		fmt.Printf("Body : %s", body)
+		fmt.Printf("POST package: %d\n", sum)
 		sum += 1;
 	}
 }
